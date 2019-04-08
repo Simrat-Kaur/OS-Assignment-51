d48a8382244ca4c15acd7b1ef4264c05c8fa6072
Simrat-Kaur <47895767+Simrat-Kaur@users.noreply.github.com>
2019-04-08 22:16:52 +0530

    Add files via upload

--- Scheduler.c
+++ Scheduler.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#define MAX_QUEUE_SIZE 100
+#define MAX_QUEUE_SIZE 100   //queue of size 100 is defined here
 typedef struct Processes {
-	int p_no;
-	int arr_time;
+	int process_no;      
+	int arrival_time;
 	int burst_time;
 } Process;
 Process * queue[MAX_QUEUE_SIZE];
-int front = 0, rear = -1, processed = 0, curr_time = 0, tq = 6;
-int last_front = 0, last_rear = -1;
+int front = 0, rear = -1, process1 = 0, current_time = 0, tq = 6;  //here tq is time quantum as the process is interrupted after 6 sec.
+int l_front = 0, l_rear = -1;
 void swap(Process * a, Process * b) {
 	Process temp = * a;
 	* a = * b;
 	* b = temp;
 }
-void sort(Process p[], int n) {
+void sort_process(Process p[], int n) {
 	int i, j;
 	short swapped;
 	for (i = 0; i < n; ++i) {
 		swapped = 0;
 		for (j = 0; j < n-i-1; ++j)
 		{
-			if (p[j].arr_time > p[j+1].arr_time)
+			if (p[j].arrival_time > p[j+1].arrival_time) 		// swap the processes if the arrival time of process p[j] is greater than the process p[j+1]
 			{
 				swap(&p[j], &p[j+1]);
 				swapped = 1;
@@ -30,60 +30,61 @@ void sort(Process p[], int n) {
 			break;
 	}
 }
+
+void execute_process() {
+	int i;
+
+	if (front-1 == rear) {
+		printf("CPU idle for 1 second.\n");
+		current_time += 1;
+	}
+	else {
+		l_front = front;
+		l_rear = rear;
+		for (i = front; i <= rear; ++i, ++front)
+		{
+			if (queue[i]->burst_time > tq)
+			{
+				queue[i]->burst_time -= tq;
+				current_time += tq;
+				printf("Process number %d excuted till %d seconds.\n", queue[i]->process_no, current_time);
+			}
+			else if (queue[i]->burst_time > 0)
+			{
+				current_time += queue[i]->burst_time;
+				queue[i]->burst_time = 0;
+				printf("Process number %d excuted till %d seconds.\n", queue[i]->process_no, current_time);
+				++process1;
+			}
+		}
+	}
+}
 void enqueue(Process p[], int n) {
-	int i, j, can_insert;
+	int i, j, insert;
 	for (i = 0; i < n; ++i)
 	{
-		can_insert = 1;
+		insert = 1;
 
-		if (p[i].arr_time <= curr_time && p[i].burst_time > 0)
+		if (p[i].arrival_time <= current_time && p[i].burst_time > 0)
 		{
 			if (front == 0) {
 				queue[++rear] = &p[i];
 			}
 			else
 			{
-				for (j = last_front; j <= last_rear; ++j) {
-					if (queue[j]->p_no == p[i].p_no)
-						can_insert = 0;
+				for (j = l_front; j <= l_rear; ++j) {
+					if (queue[j]->process_no == p[i].process_no)
+						insert = 0;
 				}
-				if (can_insert == 1)
+				if (insert == 1)
 					queue[++rear] = &p[i];
 			}
 		}
 	}
-	for (i = last_front; i <= last_rear; ++i)
+	for (i = l_front; i <= l_rear; ++i)
 	{
 		if (queue[i]->burst_time > 0)
 			queue[++rear] = queue[i];
-	}
-}
-void execute() {
-	int i;
-
-	if (front-1 == rear) {
-		printf("CPU idle for 1 second.\n");
-		curr_time += 1;
-	}
-	else {
-		last_front = front;
-		last_rear = rear;
-		for (i = front; i <= rear; ++i, ++front)
-		{
-			if (queue[i]->burst_time > tq)
-			{
-				queue[i]->burst_time -= tq;
-				curr_time += tq;
-				printf("Process number %d excuted till %d seconds.\n", queue[i]->p_no, curr_time);
-			}
-			else if (queue[i]->burst_time > 0)
-			{
-				curr_time += queue[i]->burst_time;
-				queue[i]->burst_time = 0;
-				printf("Process number %d excuted till %d seconds.\n", queue[i]->p_no, curr_time);
-				++processed;
-			}
-		}
 	}
 }
 int main() {
@@ -91,32 +92,32 @@ int main() {
 	short err_flag = 0;
 	do {
 		if (err_flag == 1)
-			fprintf(stderr,"\nNumber of processes should be greater than 1.\n");
-		printf("Enter the number of processes: ");
-		scanf("%d", &n);
+			fprintf(stderr,"\nNumber of processes should be greater than 1.\n");     //we have to enter more than one process
+		printf("Enter the number of processes: ");             
+		scanf("%d", &n);                         //we can take n number of inputs 
 		err_flag = 1;
 	} while (n < 1);
 	err_flag = 0;
 	Process p[n];
 	for (i = 0; i < n; ++i) {
 		printf("\n");
-		printf("Enter arrival time of process %d: ", i+1);
-		scanf("%d", &p[i].arr_time);
-		printf("Enter burst time of process %d: ", i+1);
+		printf("Enter Arrival time of process %d: ", i+1);      
+		scanf("%d", &p[i].arrival_time);
+		printf("Enter Burst time of process %d: ", i+1);
 		scanf("%d", &p[i].burst_time);
-		p[i].p_no = i+1;
+		p[i].process_no = i+1;
 	}
-	sort(&p[0], n); // Sort the processes according to the arrival time of each process.
+	sort_process(&p[0], n);                                              // sort_process the processes according to the arrival time of each process.
 	while (1) {
 		enqueue(p, n);
-		printf("\nIn queue: ");
+		printf("\nQueue: ");
 		for (i = 0; i <= rear; ++i) {
-			printf("%d ", queue[i]->p_no);
+			printf("%d ", queue[i]->process_no);
 		}
 		printf("\nFront = %d, Rear = %d.\n\n", front, rear);
-		execute();
-		// If all the processes have been processed, break from the loop.
-		if (processed == n)
+		execute_process();
+		                                                           // If all the processes have been process1, break from the loop.
+		if (process1 == n)
 			break;
 	}
 	return 0;
